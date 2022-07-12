3e5f91f21a958b618181d9b5d070f31f696ffc64
VincentHChoy <vhchoy@ualberta.ca>
2022-07-12 14:05:02 -0600

    adds onto application.js with dummy data using appointment components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-import Appointment from "components/Appointment";
+import Appointment from "./Appointments";
 
 export default function Application(props) {
-const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState("Monday");
   const [interviewer, setInterviewer] = useState("");
 
   const days = [
@@ -24,7 +24,57 @@ const [day, setDay] = useState("Monday");
       spots: 0,
     },
   ];
-  console.log(day)
+
+  const appointments = {
+    1: {
+      id: 1,
+      time: "12pm",
+    },
+    2: {
+      id: 2,
+      time: "1pm",
+      interview: {
+        student: "Lydia Miller-Jones",
+        interviewer: {
+          id: 3,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png",
+        },
+      },
+    },
+    3: {
+      id: 3,
+      time: "2pm",
+    },
+    4: {
+      id: 4,
+      time: "3pm",
+      interview: {
+        student: "Archie Andrews",
+        interviewer: {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg",
+        },
+      },
+    },
+    5: {
+      id: 5,
+      time: "4pm",
+    },
+  };
+
+  const appointmentList = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={appointment.interview}
+      />
+    );
+  }); 
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,11 +85,7 @@ const [day, setDay] = useState("Monday");
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
+          <DayList days={days} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -48,7 +94,7 @@ const [day, setDay] = useState("Monday");
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
       </section>
     </main>
   );
