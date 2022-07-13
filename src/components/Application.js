c2444025a28725d881bbca8761fb81f3d0a9b7a2
VincentHChoy <vhchoy@ualberta.ca>
2022-07-13 12:10:37 -0600

    adds axios and get request for days, removes hard coded data for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointments";
+import axios from "axios";
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState([]);
   const [interviewer, setInterviewer] = useState("");
+  useEffect(()=>{
+    axios.get("/api/days").then(response =>{
+      console.log(response.data)
+      setDay(response.data)
+    })
+  },[])
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
 
   const appointments = {
     1: {
@@ -85,7 +75,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={day} value={day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
