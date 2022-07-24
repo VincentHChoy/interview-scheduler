ed1c74fc81b06099d96fd26f4d0d43cf58b4f46c
VincentHChoy <vhchoy@ualberta.ca>
2022-07-24 15:47:25 -0600

    adds tests for Form.js checking for input validations for student

--- src/components/Appointments/Form.js
+++ src/components/Appointments/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList";
 function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setInterviewer(null);
@@ -12,25 +13,40 @@ function Form(props) {
     props.onCancel();
   };
 
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    props.onSave(student, interviewer);
+  }
+
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => {
               setStudent(event.target.value);
             }}
+            data-testid="student-name-input"
             /*
           This must be a controlled component
           your code goes here
         */
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -47,7 +63,8 @@ function Form(props) {
           >
             Cancel
           </Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+          <Button confirm onClick={() => 
+            validate()}>
             Save
           </Button>
         </section>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 function InterviewerList(props) {
   const list = props.interviewers.map((interviewer) => {
@@ -21,6 +22,10 @@ function InterviewerList(props) {
       <ul className="interviewers__list">{list}</ul>
     </section>
   );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
 }
 
 export default InterviewerList;
